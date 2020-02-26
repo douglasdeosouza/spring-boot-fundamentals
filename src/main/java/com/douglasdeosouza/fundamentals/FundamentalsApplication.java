608464b5da257191581022a30a536d29e24e91eb
Douglas de Oliveira Souza <douglas.de.o.souza@gmail.com>
2020-02-25 22:39:24 -0300

    firts commit create project

---
+++ src/main/java/com/douglasdeosouza/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.douglasdeosouza.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsApplication.class, args);
+    }
+
+}
